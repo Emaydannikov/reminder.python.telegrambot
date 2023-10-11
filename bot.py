41f79b7962f6fe3cef138a575cd6a17207bdc9f7
Emaydannikov <89180289+Emaydannikov@users.noreply.github.com>
2023-10-11 17:03:23 +0300

    New version bot.py

--- bot.py
+++ bot.py
@@ -1,47 +1,111 @@
+from config import TOKEN, FILENAME
 import telebot
-import json
-import threading
 from telebot import types
-from config import TOKEN
+import json
 from datetime import datetime
-from notifier import send_notifications
+import random
+import string
 
 bot = telebot.TeleBot(TOKEN)
-user_states = {}  # To track the conversation states
+user_states = {}
 
-FILENAME = "reminders.json"
+
+def generate_id(length=8):
+    chars = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(chars) for i in range(length))
+
+
+def generate_markup():
+    markup = types.InlineKeyboardMarkup()
+    item1 = types.InlineKeyboardButton("Add Reminder", callback_data="add")
+    item2 = types.InlineKeyboardButton("View Reminders", callback_data="view")
+    item3 = types.InlineKeyboardButton(
+        "Delete Reminder", callback_data="delete")
+    markup.add(item1, item2, item3)
+    return markup
+
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Hello! Use /add to add a reminder, /show to list reminders, and /delete to remove one.")
+    markup = generate_markup()
+    bot.send_message(
+        message.chat.id, "Welcome! What would you like to do?", reply_markup=markup)
 
-@bot.message_handler(commands=['add'])
-def add_reminder_step1(message):
-    user_id = message.from_user.id
-    user_states[user_id] = {"step": 1}
-    bot.reply_to(message, "Please provide the reminder text.")
+
+@bot.callback_query_handler(func=lambda call: True)
+def handle_query(call):
+    if call.data == "add":
+        bot.answer_callback_query(call.id)
+        user_states[call.from_user.id] = {"step": 1}
+        bot.send_message(call.from_user.id,
+                         "Please provide the reminder text.")
+    elif call.data == "view":
+        bot.answer_callback_query(call.id)
+        view_reminders(call.from_user.id)
+    elif call.data == "delete":
+        bot.answer_callback_query(call.id)
+        user_states[call.from_user.id] = {"step": "delete"}
+        bot.send_message(
+            call.from_user.id, "Please provide the ID of the reminder you want to delete.")
+
+
+def view_reminders(user_id):
+    reminders = []
+    try:
+        with open(FILENAME, 'r') as f:
+            reminders = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        pass
+
+    user_reminders = [
+        reminder for reminder in reminders if reminder["user_id"] == user_id]
+    if not user_reminders:
+        bot.send_message(user_id, "You have no reminders.")
+        return
+
+    for reminder in user_reminders:
+        bot.send_message(user_id, f"ID: {reminder['id']}\nReminder: {reminder['reminder_text']}\nFrequency: Every {
+                         reminder['frequency_hours']} hour(s)\nTimes to fire: {reminder['times_to_fire']} times\nStatus: {reminder['status']}")
+
 
 @bot.message_handler(func=lambda message: user_states.get(message.from_user.id, {}).get("step") == 1)
 def add_reminder_step2(message):
     user_id = message.from_user.id
-    user_states[user_id]["step"] = 2
     user_states[user_id]["reminder_text"] = message.text
-    bot.reply_to(message, "How often should I remind you? (e.g., 2h, 4h, 24h)")
+    bot.send_message(
+        user_id, "How often should I remind you? (e.g., 2h, 4h, 24h)")
+    user_states[user_id]["step"] = 2
+
 
 @bot.message_handler(func=lambda message: user_states.get(message.from_user.id, {}).get("step") == 2)
 def add_reminder_step3(message):
     user_id = message.from_user.id
     frequency = message.text.rstrip("h")
-
     if frequency.isdigit():
-        frequency_hours = int(frequency)
+        user_states[user_id]["frequency_hours"] = int(frequency)
+        bot.send_message(
+            user_id, "How many times should the notification fire? (Input a number or 'infinite')")
+        user_states[user_id]["step"] = 3
+    else:
+        bot.send_message(
+            user_id, "Invalid format. Please enter a valid frequency (e.g., 2h, 4h, 24h).")
 
-        creation_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+@bot.message_handler(func=lambda message: user_states.get(message.from_user.id, {}).get("step") == 3)
+def add_reminder_step4(message):
+    user_id = message.from_user.id
+    if message.text.isdigit() or message.text == "infinite":
+        times_to_fire = int(message.text) if message.text.isdigit() else 9999
+        user_states[user_id]["times_to_fire"] = times_to_fire
+
         reminder = {
+            "id": generate_id(),
             "user_id": user_id,
-            "creation_time": creation_time,
+            "creation_time": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
             "reminder_text": user_states[user_id]["reminder_text"],
-            "frequency_hours": frequency_hours
+            "frequency_hours": user_states[user_id]["frequency_hours"],
+            "times_to_fire": user_states[user_id]["times_to_fire"],
+            "status": "not completed"
         }
 
         reminders = []
@@ -56,15 +120,19 @@ def add_reminder_step3(message):
         with open(FILENAME, 'w') as f:
             json.dump(reminders, f)
 
-        bot.reply_to(message, f"Added reminder: {reminder['reminder_text']} to remind every {frequency_hours} hour(s).")
-        del user_states[user_id]  # Remove user state after completing the process
-
+        bot.send_message(user_id, f"Added reminder: {reminder['reminder_text']} to remind every {
+                         reminder['frequency_hours']} hour(s) for {reminder['times_to_fire']} times.")
+        del user_states[user_id]
+        send_welcome(message)
     else:
-        bot.reply_to(message, "Invalid format. Please enter a valid frequency (e.g., 2h, 4h, 24h).")
+        bot.send_message(
+            user_id, "Invalid input. Please enter a valid number or 'infinite'.")
 
-@bot.message_handler(commands=['show'])
-def show_reminders(message):
-    user_id = message.from_user.id 
+
+@bot.message_handler(func=lambda message: user_states.get(message.from_user.id, {}).get("step") == "delete")
+def delete_reminder(message):
+    user_id = message.from_user.id
+    reminder_id = message.text
 
     reminders = []
     try:
@@ -73,46 +141,17 @@ def show_reminders(message):
     except (FileNotFoundError, json.JSONDecodeError):
         pass
 
-    user_reminders = [r for r in reminders if r['user_id'] == user_id]
-
-    if user_reminders:
-        reminder_texts = [f"Text: {r['reminder_text']} | Frequency: {r['frequency_hours']} hours | Created: {r['creation_time']}" for r in user_reminders]
-        reminder_list = '\n'.join(reminder_texts)
-        bot.reply_to(message, reminder_list)
+    reminder_to_delete = next(
+        (reminder for reminder in reminders if reminder["id"] == reminder_id and reminder["user_id"] == user_id), None)
+    if reminder_to_delete:
+        reminders.remove(reminder_to_delete)
+        with open(FILENAME, 'w') as f:
+            json.dump(reminders, f)
+        bot.send_message(user_id, f"Deleted reminder: {
+                         reminder_to_delete['reminder_text']}")
+        del user_states[user_id]
     else:
-        bot.reply_to(message, "You have no reminders set.")
+        bot.send_message(user_id, "No such reminder found.")
 
-@bot.message_handler(commands=['delete'])
-def delete_reminder(message):
-    index_text = message.text.split("/delete ", 1)[-1]
-    if index_text.isdigit():
-        index = int(index_text)
-        with open(FILENAME, 'r') as f:
-            reminders = json.load(f)
-        if 0 <= index < len(reminders):
-            removed = reminders.pop(index)
-            with open(FILENAME, 'w') as f:
-                json.dump(reminders, f)
-            bot.reply_to(message, f"Deleted reminder: {removed}")
-        else:
-            bot.reply_to(message, "Please provide a valid reminder index to delete.")
-    else:
-        bot.reply_to(message, "Please provide a valid reminder index to delete.")
 
-@bot.callback_query_handler(func=lambda call: call.data.startswith("done_"))
-def callback_query(call):
-    _, user_id, creation_time = call.data.split("_")
-    with open('reminders.json', 'r') as f:
-        reminders = json.load(f)
-
-    reminders = [reminder for reminder in reminders if not (reminder['user_id'] == int(user_id) and reminder['creation_time'] == creation_time)]
-
-    with open('reminders.json', 'w') as f:
-        json.dump(reminders, f)
-
-    bot.answer_callback_query(call.id, "Reminder marked as done!")
-
-if __name__ == '__main__':
-    t = threading.Thread(target=send_notifications, args=(bot,))
-    t.start()
-    bot.polling(none_stop=True)
+bot.polling(none_stop=True)
